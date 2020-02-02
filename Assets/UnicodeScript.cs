5daacd58a2c4b94427d6a745a324fc607f10cf37
Markus Pettersson <markus05.pettersson@gmail.com>
2020-02-02 17:59:36 +0100

    Added script

---
+++ Assets/UnicodeScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class UnicodeScript : MonoBehaviour
+{
+    public KMAudio Audio;
+    public KMBombModule Module;
+    public KMBombInfo Info;
+
+    public KMSelectable[] Buttons;
+    public KMSelectable UPlusButton;
+
+    public TextMesh[] SymbolsScreen;
+    public TextMesh TextArray;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId = 0;
+    private bool isSolved = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _moduleId = _moduleIdCounter++;
+        Module.OnActivate += Activate;
+    }
+
+    void Activate()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
